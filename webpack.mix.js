57522634b1a6d7e3f7ccea1121ea0b1368b67ea9
Daryl Knight <github@codeknight.co.uk>
2022-04-26 11:24:29 +0300

    Renamed /dist to /assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,23 @@
-let mix = require('laravel-mix');
-const baseUrl = process.env.PRIMARY_SITE_URL; // Get value from .env to use as Browsersync proxy URL
+let mix = require('laravel-mix')
+const baseUrl = process.env.PRIMARY_SITE_URL // Get value from .env to use as Browsersync proxy URL
 
-mix.setPublicPath('./public/dist')
+mix.setPublicPath('./public/assets')
 
 	.combine(
 		[
 			'node_modules/lazysizes/lazysizes.js', // Used for lazy loading images
 		],
-		'public/dist/js/vendor.js'
+		'public/assets/js/vendor.js'
 	)
 
-	.js('src/js/app.js', 'public/dist/js')
-	.postCss('src/pcss/app.pcss', 'public/dist/css', [require('tailwindcss')])
+	.js('src/js/app.js', 'public/assets/js')
+	.postCss('src/pcss/app.pcss', 'public/assets/css', [require('tailwindcss')])
 
 	.browserSync({
-		files: ['public/dist/css/*', 'public/dist/js/*', 'templates/**/*'], // BrowserSync will refresh every time one of these files changes
+		files: ['public/assets/css/*', 'public/assets/js/*', 'templates/**/*'], // BrowserSync will refresh every time one of these files changes
 		proxy: baseUrl, // This takes PRIMARY_SITE_URL from the .env file so devs can have different local URLs
 		notify: false,
 	})
 
 	.version()
-	.disableNotifications();
+	.disableNotifications()
