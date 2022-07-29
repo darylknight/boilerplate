e6423068a9917340e306362caa55b2290ab79e94
Daryl Knight <github@codeknight.co.uk>
2022-07-29 11:10:57 +0300

    Allow updates locally

--- config/general.php
+++ config/general.php
@@ -29,7 +29,7 @@ return [
 		'disallowRobots' => !$isProd, // Stop robots crawling the site if it's not production
 		'allowAdminChanges' => $isDev, // Don't allow administrative changes on staging or production
 		'devMode' => !$isProd, // Enable devMode in local and staging, but not production
-		'allowUpdates' => false, // Never allow auto updates. Do that from Composer
+		'allowUpdates' => $isDev, // Allow plugin installs and updates on local, but not staging or production
 		'enableTemplateCaching' => $isProd, // Only allow template caching on production
 		'limitAutoSlugsToAscii' => true, // https://craftcms.com/docs/3.x/config/config-settings.html#limitautoslugstoascii
 
