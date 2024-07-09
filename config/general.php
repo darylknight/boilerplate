6f2a83372bd802134440eef8481df2ec00f94e10
Daryl Knight <darylknight@users.noreply.github.com>
2024-07-09 16:35:59 +0300

    Update format of general.php, remove unnecessary settings in Craft 5

--- config/general.php
+++ config/general.php
@@ -8,50 +8,39 @@
  * @see \craft\config\GeneralConfig
  */
 
+use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-$isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
-$isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
+return GeneralConfig::create()
+	->defaultWeekStartDay(1) // Set the default week start day for date pickers
+	->omitScriptNameInUrls() // Whether generated URLs should omit "index.php"
+	->cpTrigger('control') // The URI segment that tells Craft to load the control panel
+	->sendPoweredByHeader(false) // Don't tell the world that this site uses Craft
+	->maxUploadFileSize(100000000) // 100mb upload limit
+	->useEmailAsUsername() // Uses email instead of username
+	->preloadSingles() // Preload Single entries as Twig variables
+	->preventUserEnumeration() // Prevent user enumeration attacks
+	->enableGql(false) // Remove GraphQL from the sidebar
+	->limitAutoSlugsToAscii() // https://craftcms.com/docs/5.x/reference/config/general.html#limitautoslugstoascii
+	->previewTokenDuration('P1M') // Make preview links (?x-craft-preview) last for a month instead of a day
+	->upscaleImages(false) // don't upscale image transforms
 
-return [
-	'*' => [
-		'defaultWeekStartDay' => 1, // Monday
-		'omitScriptNameInUrls' => true, // Whether generated URLs should omit "index.php"
-		'cpTrigger' => App::env('CP_TRIGGER') ?: 'control', // The URI segment that tells Craft to load the control panel
-		'securityKey' => App::env('CRAFT_SECURITY_KEY'), // The secure key Craft will use for hashing and encrypting data
-		'addTrailingSlashesToUrls' => false, // Don't add trailing slashes
-		'sendPoweredByHeader' => false, // Don't tell the world that this site uses Craft
-		'maxUploadFileSize' => 100000000, // 100mb upload limit
-		'useEmailAsUsername' => true, // Uses email instead of username
-		'preventUserEnumeration' => true, // Stop Craft from confirming if a username exists on the forgot password screen
-		'enableGql' => false, // Remove GraphQL from the sidebar
-		'transformGifs' => false, // Don't screw up gifs by transforming them
-		'disallowRobots' => !$isProd, // Stop robots crawling the site if it's not production
-		'allowAdminChanges' => $isDev, // Don't allow administrative changes on staging or production
-		'devMode' => $isDev, // Enable devMode in local and staging, but not production
-		'allowUpdates' => $isDev, // Allow plugin installs and updates on local, but not staging or production
-		'enableTemplateCaching' => $isProd, // Only allow template caching on production
-		'limitAutoSlugsToAscii' => true, // https://craftcms.com/docs/3.x/config/config-settings.html#limitautoslugstoascii
-		'previewTokenDuration' => 'P1M', // Make preview links (?x-craft-preview) last for a month instead of a day
-		'upscaleImages' => false, // don't upscale image transforms
+	// Set default accessibility options for everyone
+	->accessibilityDefaults([
+		'useShapes' => true,
+	])
 
-		// Set default accessibility options for everyone
-		'accessibilityDefaults' => [
-			'useShapes' => true,
+	// Add an SVG-only option to Asset fields (https://craftcms.com/docs/4.x/config/general.html#extrafilekinds)
+	->extraFileKinds([
+		'svg' => [
+			'label' => 'SVG',
+			'extensions' => ['svg'],
 		],
+	])
 
-		// Add an SVG-only option to Asset fields
-		'extraFileKinds' => [
-			'svg' => [
-				'label' => 'SVG',
-				'extensions' => ['svg'],
-			],
-		],
-
-		// This uses values from .env
-		'aliases' => [
-			'@web' => getenv('PRIMARY_SITE_URL'),
-			'@webroot' => getenv('BASE_PATH'),
-		],
-	],
-];
+	// This uses values from .env
+	->aliases([
+		'@web' => getenv('PRIMARY_SITE_URL'),
+		'@webroot' => dirname(__DIR__) . '/web',
+	])
+;
